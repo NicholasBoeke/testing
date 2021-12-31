5d37cb94fcfb680350eb5239b9f8ebd3ef5cc06d
Nicholas <NicholasBoeke@gmail.com>
2021-12-31 13:13:40 -0600

    Create python_script3.py
    created on main branch, after creating 'branch_test' and while branch_test was active

---
+++ python_script3.py
@@ -0,0 +1,6 @@
+"""
+this is a third python script, but it was created on the main branch after creating 'branch_test` brach, and while the testing branch was active
+"""
+
+string = 'this is a string'
+x = string.find('i')
