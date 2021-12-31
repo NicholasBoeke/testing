e488b79003ee23b09e7aad2fe5e11b55fb2173a4
Nicholas <NicholasBoeke@gmail.com>
2021-12-31 14:40:22 -0600

    added jupyter cells
    testing jupyter and git integrations in VS Code; run interactively with keyboard shortcuts

--- python_script1.py
+++ python_script1.py
@@ -1,4 +1,12 @@
+#%%
 import pandas as pd
 
+#%%
 df = pd.DataFrame({'A':[1,2,3],
                    'B':['a', 'b', 'c']})
+
+# %%
+df
+
+
+# %%
--- python_script2.py
+++ python_script2.py
@@ -1,8 +1,11 @@
 """
 this is another script
 """
-
+#%%
 import pandas as pd
 
+#%%
 df = pd.DataFrame({'C':[4,5,6],
                    'D':['d', 'e', 'f']})
+
+# %%
